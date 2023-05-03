d240dc0bf30514189e9e067d393345d6b199a2d4
Viktorr22 <progdub1@gmail.com>
2023-05-03 22:23:09 +0300

    add basename

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
+import { NavLink, Routes, Route } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <NavLink>Home</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+      </Routes>
     </div>
   );
 };
